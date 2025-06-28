81a35f72505284efb94388d383dde366bf004e7c
difonz <kevindifonzo@hotmail.com>
2025-06-27 22:49:42 -0400

    adding all files

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from stats import get_number_of_words,get_characters
-
-
-def get_book_text(filePath):
-    contents = None
-    with open(filePath) as f:
-        return f.read()
+from stats import (
+    get_number_of_words,
+    get_characters,
+    sortDict
+)
+import sys 
 
 def main():
-    bookPath = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    bookPath = sys.argv[1]
     text = get_book_text(bookPath)
-    print(text)
-    numberOfWords =get_number_of_words(text)
-    print(f"{numberOfWords} words found in the document")
+    wordNum = get_number_of_words(text)
+    charDict = get_characters(text)
+    sortedCharList = sortDict(charDict)
+    printReport(bookPath,wordNum,sortedCharList)
     
-    charactersInText = get_characters(text)
-    print(f"{charactersInText}")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
     
-if __name__ == "__main__":
-    main()
+def printReport(bookPath,wordNum, sortedCharList):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {bookPath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wordNum} total words")
+    print("--------- Character Count -------")
+    for item in sortedCharList:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+    
+main()
--- stats.py
+++ stats.py
@@ -4,7 +4,22 @@ def get_number_of_words(text):
 
 def get_characters(text):
     charDict = {}
-    for letter in text:
-        lowerChar = letter.lower()
-        charDict[lowerChar] = charDict.get(lowerChar,0)+1
+    for c in text:
+        lowered = c.lower()
+        if lowered in charDict:
+            charDict[lowered] +=1
+        else:
+            charDict[lowered]=1
     return charDict
+
+
+def sort_on(d):
+    return d["num"]
+
+def sortDict(charDict):
+    sortedList = []
+    for char in charDict:
+        sortedList.append({"char": char, "num": charDict[char]})
+    sortedList.sort(reverse=True,key=sort_on)
+    return sortedList   
+    
